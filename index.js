479fb2531d239a3043c5a07f4fbd760fc7a4e001
Keino Chichester <keino.chichester@gmail.com>
2023-01-12 00:01:51 -0500

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-// Write your code here
+const num1 = 1
+const num2 = 62
+
+const multiply = num1 * num2
+
+console.log(multiply)
+
+const random = Math.floor(Math.random() * 10);
+
+
+const num3 = 24
+const num4 = 5
+
+const mod = num3 % num4
+
+const set = [10, 15, 20]
+
+const max = Math.max(10, 15, 20)
+
+
